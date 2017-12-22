67b6ebaffd33a6f76c59d602f8523b2e57540740
Mark <marklewis@qq.com>
2017-12-22 21:27:45 +0800

    add new Func for Code Line

---
+++ CodeLineCount.h
@@ -0,0 +1,15 @@
+//
+//  CodeLineCount.h
+//  MKJToolKit
+//
+//  Created by Mark Lewis on 17-12-22.
+//  Copyright (c) 2017年 Mark Lewis. All rights reserved.
+//
+
+#ifndef __MKJToolKit__CodeLineCount__
+#define __MKJToolKit__CodeLineCount__
+
+#include <iostream>
+#import <Foundation/Foundation.h>
+NSUInteger codeLineCount(NSString *path);
+#endif /* defined(__MKJToolKit__CodeLineCount__) */
---
+++ CodeLineCount.mm
@@ -0,0 +1,9 @@
+//
+//  CodeLineCount.cpp
+//  MKJToolKit
+//
+//  Created by Mark Lewis on 17-12-22.
+//  Copyright (c) 2017年 Mark Lewis. All rights reserved.
+//
+
+#include "CodeLineCount.h"

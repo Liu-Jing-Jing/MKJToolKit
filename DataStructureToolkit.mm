2a7ccf448b24d49ac4043df1e8f0a32a1fee9ae7
Mark <marklewis@qq.com>
2017-12-23 20:13:39 +0800

    The Toolkit for HP University DS Course
    provide the OC interface for CPP
    Using Cpp and Objective-C
    This is the Toolkit for HP University DS Course

---
+++ DataStructureToolkit.mm
@@ -0,0 +1,21 @@
+//
+//  DataStructureToolkit.c
+//  MKJToolKit
+//
+//  Created by Mark Lewis on 17-12-22.
+//  Copyright (c) 2017年 Mark Lewis. All rights reserved.
+//
+#import "DataStructureToolkit.h"
+
+@implementation DataStructureToolkit
+
++(void)SqListTestcase
+{
+    SqListTestcase();
+}
+
++ (void)SqListMergeTestcase
+{
+    SqListMergeTestcase();
+}
+@end
